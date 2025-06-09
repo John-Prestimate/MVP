2f04618c61a0676d0f410a9c8cca4c69369f5ad0
John Phillips <your@email.com>
2025-06-09 15:51:41 -0500

    Updated

--- my-prestimate-app/src/App.tsx
+++ my-prestimate-app/src/App.tsx
@@ -1,3 +1,4 @@
+import Dashboard from "./components/Dashboard";
 import React from 'react';
 import MapView from "./components/MapView"; // ✅ CORRECT
 
@@ -5,6 +6,7 @@ function App() {
   return (
     <div>
       <h1>Welcome to Prestimate</h1>
+      <Dashboard />
       <MapView />
     </div>
   );
