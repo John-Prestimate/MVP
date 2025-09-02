0ed820214d00dfc7ec409aa2f658537fa4f7bb97
John Phillips <j_phillips25@hotmail.com>
2025-09-01 19:45:44 -0500

    Clean up main.tsx: remove DemoMapPage and conditional logic

--- my-prestimate-app/src/main.tsx
+++ my-prestimate-app/src/main.tsx
@@ -1,26 +1,15 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import DemoMapPage from './DemoMapPage';
 import { MantineProvider } from '@mantine/core';
 import App from './App';
 
-const path = window.location.pathname;
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement!);
 
-if (path === '/demo-map') {
-  root.render(
-    <React.StrictMode>
-      <DemoMapPage />
-    </React.StrictMode>
-  );
-} else {
-  root.render(
-    <React.StrictMode>
-      <MantineProvider>
-        <App />
-      </MantineProvider>
-    </React.StrictMode>
-  );
-}
+root.render(
+  <React.StrictMode>
+    <MantineProvider>
+      <App />
+    </MantineProvider>
+  </React.StrictMode>
+);
